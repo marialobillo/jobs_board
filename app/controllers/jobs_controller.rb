990c6dd772f00f8a641cea63db95a80aad9c0bef
Neniah <maria.lobillo.santos@gmail.com>
2015-07-06 20:10:18 +0200

    jobs form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,44 @@
 class JobsController < ApplicationController
+  before_action :find_job, only: [:show, :edit, :update, :destroy]
 
   def index
 
   end
-  
+
+  def show
+
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(jobs_params)
+
+    if @job.save
+      redirect_to @job
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def jobs_parms
+      params.require(:job).permit(:title, :description, :company, :url)
+    end
+
+    def find_job
+      @job = Job.find(params[:id])
+    end
 end
